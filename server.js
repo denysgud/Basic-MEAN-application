f089f7a0290c55604fead03a6e540fd458b82f17
Denys Gudakov <denysgud@gmail.com>
2016-09-27 13:05:56 +0300

    Configured environment configuration file

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var app = express();
 
-app.use('/', function(req, res) {
-  res.send('Hello World');
-});
+console.log(process.env.NODE_ENV);
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+console.log(process.env.NODE_ENV);
+
+var express = require('./config/express');
+var app = express();
 
 app.listen(3000);
 
-console.log('Server running at http://localhost:3000/');
-
 module.exports = app;
+
+console.log('Server running at http://localhost:3000/');
