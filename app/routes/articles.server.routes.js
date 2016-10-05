d1c6531c3d627b209d984c298ee549421cdc206b
Denys Gudakov <denysgud@gmail.com>
2016-10-05 12:34:32 +0300

    Implemented MEAN CRUD model

--- app/routes/articles.server.routes.js
+++ app/routes/articles.server.routes.js
@@ -1,14 +1,15 @@
+var users = require('../../app/controllers/users.server.controller');
 var articles = require('../../app/controllers/articles.server.controller');
 
 module.exports = function(app) {
-  app.route('/articles')
-    .post(articles.create)
+  app.route('/api/articles')
+    .post(users.requiresLogin, articles.create)
     .get(articles.list);
 
-  app.route('/articles/:articleId')
+  app.route('/api/articles/:articleId')
     .get(articles.read)
-    .put(articles.update)
-    .delete(articles.delete);
+    .put(users.requiresLogin, articles.update)
+    .delete(users.requiresLogin, articles.delete);
 
   app.param('articleId', articles.articleByID);
 };
