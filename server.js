6e8c1953f3f2603140c993798a88548209613812
Denys Gudakov <denysgud@gmail.com>
2016-10-06 13:29:37 +0300

    Small fixes for heroku configuration

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.PORT = process.env.PORT || 8080;
 
 var mongoose = require('./config/mongoose'); // must be loaded before any other configuration
 var express = require('./config/express');
@@ -8,8 +9,8 @@ var db = mongoose();
 var app = express(db);
 var passport = passport();
 
-app.listen(3000);
+app.listen(process.env.PORT);
 
 module.exports = app;
 
-console.log('Server running at http://localhost:3000/');
+console.log('Server running at http://localhost:' + process.env.PORT + '/');
