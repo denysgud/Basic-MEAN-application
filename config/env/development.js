085dbc772de684fb0e291682aa6ddba9927f8f6b
Denys Gudakov <denysgud@gmail.com>
2016-10-06 12:54:13 +0300

    Changed DB credentials for heroku-mLab connection

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,7 @@
 module.exports = {
   // db: 'mongodb://localhost/testDB',
-  db: 'mongodb://admin:password@ds033096.mlab.com:33096/basic_mean_app',
+  // db: 'mongodb://admin:password@ds033096.mlab.com:33096/basic_mean_app',
+  db: 'mongodb:heroku_z1dq6pkg:2ql1c76shqfgfsv5q1r1t1pb1f@ds035046.mlab.com:35046/heroku_z1dq6pkg',
   sessionSecret: 'developmentSessionSecret',
   facebook: {
     clientID: '545155692340221',
